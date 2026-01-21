c3ed0d9f6337f8c0a265ec2b72ff83b9e8416f33
Jorge-Tanguila <jorgrtanguila@gmail.com>
2026-01-21 16:34:19 +0100

    montado kk

--- CBA/src/pages/Bot.jsx
+++ CBA/src/pages/Bot.jsx
@@ -18,8 +18,8 @@ export default function Bot() {
   };
 
   const getBotResponseFromOpenRouter = async (message) => {
-    const apiKey = import.meta.env.VITE_OPENROUTER_API_KEY; 
-
+    //const apiKey = import.meta.env.VITE_OPENROUTER_API_KEY; 
+    const apiKey = import.meta.env.VITE_OPENROUTER_API_KEY;
     if (!apiKey) {
       return "Erro: Chave da API não configurada. Configure a variável VITE_OPENROUTER_API_KEY no .env";
     }
